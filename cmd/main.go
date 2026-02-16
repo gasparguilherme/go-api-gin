fecaebcb2684431ab61587afe90faee2f72036d3
Guilherme Gaspar <guilhermegaspar474@gmail.com>
2026-02-16 15:41:54 -0300

    preparando ambiente gin

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-	fmt.Println("Rodando API !")
+	server := gin.Default()
+
+	server.GET("/ping", func(ctx *gin.Context) {
+		ctx.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+
+	server.Run(":8000")
+
 }
